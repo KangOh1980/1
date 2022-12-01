b517246bc8a16dff87bb2442328c760c590f2df5
KangOh1980 <dmcddungc@gmail.com>
2022-11-30 22:38:08 -0500

    third

--- git/src/Main.java
+++ git/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!1");
-        System.out.println("Hello");
+        System.out.println("Hello yo");
+
     }
 }
